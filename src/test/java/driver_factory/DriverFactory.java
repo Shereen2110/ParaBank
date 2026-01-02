b411e06b6064451607826245c53a87c8377972fb
Shereen2110 <shyrynabrahymahmd35@gmail.com>
2026-01-02 21:50:56 +0200

    DriverFactory

---
+++ src/test/java/driver_factory/DriverFactory.java
@@ -0,0 +1,23 @@
+package driver_factory;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverFactory {
+    public static WebDriver driver;
+    public static WebDriver getWebDriver(String browserName)
+    {
+        switch (browserName.toLowerCase())
+        {
+            case "chrome":
+                driver=ChroumeDriverObject.getChroumDriver();
+                break;
+            case "firefox":
+                driver= FireFoxDriverObject.getFireFoxDriver();
+                break;
+            default:
+                driver=EdgeDriverOpject.getEdgeDriver();
+                break;
+        }
+        return driver;
+    }
+}
