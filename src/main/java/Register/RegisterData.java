51044ec7856cf92d715c4a991a3746ff2e05b1ae
Shereen2110 <shyrynabrahymahmd35@gmail.com>
2026-01-02 21:55:05 +0200

    RegisterData

---
+++ src/main/java/Register/RegisterData.java
@@ -0,0 +1,15 @@
+package Register;
+
+public class RegisterData {
+
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String city;
+    public String state;
+    public String zipCode;
+    public String phone;
+    public String ssn;
+    public String username;
+    public String password;
+}
