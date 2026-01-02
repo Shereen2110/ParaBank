95c46b0e539612bf8b6e2df1de4e91d9850f88e4
Shereen2110 <shyrynabrahymahmd35@gmail.com>
2026-01-02 21:48:25 +0200

    BillPayData

---
+++ src/main/java/BillPay/BillPayData.java
@@ -0,0 +1,60 @@
+package BillPay;
+
+public class BillPayData {
+
+    private String payeeName;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phone;
+    private String account;
+    private String verifyAccount;
+    private String amount;
+
+    // Getters
+    public String getPayeeName()
+    {
+        return payeeName;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public String getZipCode()
+    {
+        return zipCode;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public String getAccount()
+    {
+        return account;
+    }
+
+    public String getVerifyAccount()
+    {
+        return verifyAccount;
+    }
+
+    public String getAmount()
+    {
+        return amount;
+    }
+}
