4185c7ffdf4ee07bc321c602322ab9016705b081
Shereen2110 <shyrynabrahymahmd35@gmail.com>
2026-01-02 21:53:01 +0200

    FireFoxDriverObject

---
+++ src/test/java/driver_factory/FireFoxDriverObject.java
@@ -0,0 +1,13 @@
+package driver_factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxDriverObject {
+    public static WebDriver getFireFoxDriver()
+    {
+        WebDriver driver =new FirefoxDriver();
+        // WebDriver driver =new org.openqa.selenium.chrome.ChromeDriver(options);//take this path as you call this same to name of class
+        return driver;
+    }
+}
